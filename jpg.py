40e9551d9b574ff1f8c3ffd492a81203941732b8
Jing Li <jingli18@bu.edu>
2018-09-16 19:32:44 -0400

    all set(cannot fix keyword)

--- jpg.py
+++ jpg.py
@@ -2,8 +2,8 @@ from urllib import request
 
 urls=['http://pbs.twimg.com/media/DnP0kNcVAAAHhHC.jpg','http://pbs.twimg.com/media/DnPk-p7U0AAuozP.jpg']
 
-for i in range(len(urls)):
-	print(urls[i])
+for i in urls:
+	print(i)
 
-	jpg_link = urls[i]  #图片链接
-	request.urlretrieve(jpg_link, '/home/ece-student/Desktop/'+'%s.jpg' %i)
+	jpg_link = i  #图片链接
+	request.urlretrieve(jpg_link, '/home/ece-student/Desktop/api/'+'%s.jpg' %0)
