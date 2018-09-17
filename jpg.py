17438fd6508a1cc8292e3a45a5f9c6c821e705c0
Jing Li <jingli18@bu.edu>
2018-09-16 23:45:09 -0400

    Jpg edit

--- jpg.py
+++ jpg.py
@@ -6,4 +6,4 @@ for i in urls:
 	print(i)
 
 	jpg_link = i  #图片链接
-	request.urlretrieve(jpg_link, '/home/ece-student/Desktop/api/'+'%s.jpg' %0)
+	request.urlretrieve(jpg_link, '/home/ece-student/Desktop/api/'+'%s.jpg' %i)
