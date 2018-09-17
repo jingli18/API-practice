750a37ea4822aab74d6c40776f632996ff0ea4f8
Jing Li <jingli18@bu.edu>
2018-09-17 17:14:38 -0400

    all set(size a little weird)

--- Google Vision/testgg.py
+++ Google Vision/testgg.py
@@ -9,7 +9,7 @@ from google.cloud.vision import types
 client = vision.ImageAnnotatorClient()
 for n in range(16):
 	imgfile=str(n)+'.jpg'
-	pathimg=os.path.join('/home/ece-student/Desktop/api/',imgfile)
+	pathimg=os.path.join('/home/ece-student/Desktop/jpg/',imgfile)
 # The name of the image file to annotate
 	file_name = os.path.join(
  	os.path.dirname(__file__),
@@ -32,16 +32,14 @@ for n in range(16):
 
 	def add_num(img):
 		draw = ImageDraw.Draw(img)
-		ttFont = ImageFont.truetype('/home/ece-student/Desktop/Important.ttf', size=45)
+		ttFont = ImageFont.truetype('/home/ece-student/Desktop/Important.ttf', size=55)
 		fillcolor = "#3498DB"
-		width = 1500
-		height = 1000
-		img = img.resize((width,height), Image.ANTIALIAS)
+		width, height= img.size
 		i = 0
 		for label in labels:
-			draw.text((-1490, 90+i), label.description, fill=fillcolor, font=ttFont)
+			draw.text((width-1490, height-990+i), label.description, fill=fillcolor, font=ttFont)
 		#	img.save('result.jpg','jpeg')
-			i=i+50
+			i=i+60
 		savepath = os.path.join('/home/ece-student/Desktop/video/',imgfile)
 		img.save(savepath)
 		return 0
@@ -51,6 +49,5 @@ for n in range(16):
 		image = Image.open(pathimg)
 		add_num(image)
 	#	image.show()
-	savepath = os.path.join('/home/ece-student/Desktop/',)
 		
 	n=n+1
