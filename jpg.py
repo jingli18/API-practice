f5557a34644e657f2fd145895fee4aaed4b79186
Jing Li <jingli18@bu.edu>
2018-09-22 23:58:14 -0400

    files manage(not done)

--- jpg.py
+++
@@ -1,9 +0,0 @@
-from urllib import request
-
-urls=['http://pbs.twimg.com/media/DnP0kNcVAAAHhHC.jpg','http://pbs.twimg.com/media/DnPk-p7U0AAuozP.jpg']
-
-for i in urls:
-	print(i)
-
-	jpg_link = i  #图片链接
-	request.urlretrieve(jpg_link, '/home/ece-student/Desktop/api/'+'%s.jpg' %i)
